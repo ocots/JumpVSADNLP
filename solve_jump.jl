bc6cdee23b829a8e05ff3a423c9daf19f71f46b6
Baptiste Combelles <baptiste.combelles@proton.me>
2023-06-16 13:45:00 +0200

    disc

--- solve_jump.jl
+++ solve_jump.jl
@@ -15,12 +15,18 @@ function jump_solve(sys)
     println("Objective value = ", objective_value(sys), "\n")
 
     # Retrieves values (including duals - sign convention according to Pontrjagin max principle)
+    # Δtt = value.(Δt)
+    # t = (1:N+1)*Δtt; t = (t[1:end-1] + t[2:end])/2.0
+    # rr = value.(r); rr = (rr[1:end-1] + rr[2:end])/2.0
+    # vv = value.(v); vv = (vv[1:end-1] + vv[2:end])/2.0
+    # mm = value.(m); mm = (mm[1:end-1] + mm[2:end])/2.0
+    # uu = value.(u); uu = (uu[1:end-1] + uu[2:end])/2.0
+    # x =  [ [ rr[i], vv[i], mm[i] ] for i in 1:N ]
+    # p = -[ [ dual(con_dr[i]), dual(con_dv[i]), dual(con_dm[i]) ] for i in 1:N ];
     Δtt = value.(Δt)
     t = (1:N+1)*Δtt; t = (t[1:end-1] + t[2:end])/2.0
-    rr = value.(r); rr = (rr[1:end-1] + rr[2:end])/2.0
-    vv = value.(v); vv = (vv[1:end-1] + vv[2:end])/2.0
-    mm = value.(m); mm = (mm[1:end-1] + mm[2:end])/2.0
-    uu = value.(u); uu = (uu[1:end-1] + uu[2:end])/2.0
-    x =  [ [ rr[i], vv[i], mm[i] ] for i in 1:N ]
-    p = -[ [ dual(con_dr[i]), dual(con_dv[i]), dual(con_dm[i]) ] for i in 1:N ];
+    xx₁ = value.(x₁); xx₁ = (xx₁[1:end-1] + xx₁[2:end])/2.0
+    xx₂ = value.(x₂); xx₂ = (xx₂[1:end-1] + xx₂[2:end])/2.0
+    x =  [ [ xx₁[i], xx₂[i]] for i in 1:N ]
+    p = -[ [ dual(con_dx₁[i]), dual(con_dx₂[i])] for i in 1:N ]; 
 end
