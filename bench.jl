f89e639ad014e20e503800462561ce9aa2c5cd98
Baptiste Combelles <baptiste.combelles@proton.me>
2023-06-30 17:26:07 +0200

    foo

--- bench.jl
+++ bench.jl
@@ -6,31 +6,35 @@ using JuMP
 
 # we compare this problem: https://control-toolbox.org/CTProblems.jl/stable/problems/double_integrator_time.html#DIT
 
-include("models.jl")
+include("get_models.jl")
 
 adnlp, λa, xuv0  = get_adnlp_model()
-adnlp1, λa1, xuv01  = get_adnlp_model1()
-adnlp2, λa2, xuv02  = get_adnlp_model2()
-
 jump_nlp, λj, _  = get_jump_model()
-jump_nlp2, λj2, _  = get_jump_model2()
+
+println("Hessian of Lagrangian")
+println("JuMP")
+hess(jump_nlp, xuv0, λj)
+@btime hess(jump_nlp, xuv0, λj); # Hessian of Lagrangian
+println("Jacobian of constraints")
+println("JuMP")
+jac(jump_nlp, xuv0)
+@btime jac(jump_nlp, xuv0); # Jacobian of constraints
+
 
 println("Hessian of Lagrangian")
 println("ADNLP")
-hess(adnlp2, xuv0, λa)
-@btime hess(adnlp2, xuv0, λa)
-println("JuMP")
-hess(jump_nlp2, xuv0, λj)
-@btime hess(jump_nlp2, xuv0, λj); # Hessian of Lagrangian
-
-# compare jacobian of constraints
+hess(adnlp, xuv0, λa)
+@btime hess(adnlp, xuv0, λa)
 println("Jacobian of constraints")
 println("ADNLP")
-jac(adnlp2, xuv0)
-@btime jac(adnlp2, xuv0) # Jacobian of constraints
-println("JuMP")
-jac(jump_nlp2, xuv0)
-@btime jac(jump_nlp2, xuv0); # Jacobian of constraints
+jac(adnlp, xuv0)
+@btime jac(adnlp, xuv0) 
 
-#
+# compare jacobian of constraints
+println("JuMP")
+jac(jump_nlp, xuv0)
+@btime jac(jump_nlp, xuv0); # Jacobian of constraints
+
+
+println(sum(sum(hess(adnlp, xuv0, λa)-hess(jump_nlp, xuv0, λj))))
 nothing
